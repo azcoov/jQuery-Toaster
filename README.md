e3f3295c5ad43c13fe61e7b9f339384ba8df8e06
unknown <bcoover@.(none)>
2011-01-31 19:27:21 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Toaster
+
+## Usage
+
+Include jQuery, /toaster.js and .toaster.css.
+
+Add an empty div with an id of toaster just after the body tag of your page.
+
+<pre>
+	<div id="toaster"></div>
+</pre>
+
+Initialize the toaster.
+
+	$('#toaster').toaster();
+
+The toaster will automatically side-down if it detects any text inside the toaster div. So if you want the notification to automatically display when a page first loads, make sure to add text insde that div. Here is an example of that using asp.net mvc:
+
+	<div id="toaster"><%: TempData["notification"]%></div>
+
+If you want to pop the toaster later on after the toaster has been initialized, you can call toaster on an element, calling the show method and supplying it with a message:
+
+	$('#somelink').click(function() {
+		$(this).toaster('show', 'Hello world!');
+	});
+	
+### Customization
+
+You can change the default color of the toaster by supplying a backgroundColor.
+
+    $('#toaster').toaster({ backgroundColor: '#666699' });	
---
+++ toaster.js
@@ -0,0 +1,73 @@
+/* 
+	Toaster  - a jQuery slidedown alert notifier	
+	2011 Billy Coover
+	Copyrights are lame!
+	www.coovtech.com	
+*/
+(function ($) {
+    $.fn.extend({
+        //plugin name - toaster
+        toaster: function (options, arg) {
+            var settings = null
+            //Check that options is an object and not a method name
+            if (options && typeof (options) == 'object') {
+                options = $.extend({}, $.toaster.defaults, options);
+            } else if (!options) { //If options is a methiod name, use the default settings
+                options = $.extend({}, $.toaster.defaults, null);
+            }
+            this.each(function () {
+                new $.toaster(this, options, arg);
+            });
+            return;
+        }
+    });
+
+    $.toaster = function (elem, options, arg) {
+        if (options && typeof (options) == 'string') {
+            if (options == 'show') {
+                show(elem, arg);
+            }
+            return;
+        } else {
+            var showToaster = false;
+            var text = $(elem).html();
+            if (text != null && text != '') {
+                $(elem).html('');
+                showToaster = true;
+            }
+            init(elem, arg, options);
+            if (showToaster) {
+                show(elem, text);
+            }
+        }
+
+        function init(elem, arg, options) {
+            $(elem).html($.toaster.defaults.toasterHtml);
+            $(".toaster-bar-bkg").css('backgroundColor', options.backgroundColor);
+        }
+
+        function show(elem, arg) {
+            $(".toast-info").html(arg);
+            $(".toaster-bar-container").show('slide', { direction: 'up' }, 1000).delay(2500).hide('slide', { direction: 'up' }, 1000);
+            return false;
+        }
+    };
+
+    $.toaster.defaults = {
+        notificationContainer: "",
+        backgroundColor: '#fff',
+        color: '#000',
+        message: '',
+        toasterHtml: '\
+			<div id="toaster-area"> \
+				<div class="toaster-bar-container" style="display:none;"> \
+					<div class="toaster-bar-bkg" style="display: block; height: 22px; "></div> \
+					<div class="toaster-bar" style="display: block; "> \
+						<div class="toaster-bar-contents"> \
+							<div class="toast toast-info" ></div> \
+						</div> \
+					</div> \
+				</div> \
+			</div>'
+    };
+})(jQuery);
